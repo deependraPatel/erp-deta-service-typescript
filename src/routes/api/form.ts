36bea1fb9965bb48412d4e4e709df41a14c15710
Deependra Patel <ideependra.patel@gmail.com>
2022-07-25 17:43:55 +0530

    router for forms request

--- src/routes/api/form.ts
+++ src/routes/api/form.ts
@@ -1,16 +1,31 @@
 import express from "express";
+import { Forms } from "../../models/forms";
 
 export const router = express.Router();
 
 router.post(
-  "/",
-  async (
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ) => {
-    res.ok("Form post");
-  }
+	"/",
+	async (
+		req: express.Request,
+		res: express.Response,
+		next: express.NextFunction
+	) => {
+		const form = await Forms.create(req.body);
+		res.ok(form);
+	}
+);
+
+router.get(
+	"/",
+	async (
+		req: express.Request,
+		res: express.Response,
+		next: express.NextFunction
+	) => {
+		const forms = await Forms.find();
+
+		res.ok(forms);
+	}
 );
 
 export default router;
